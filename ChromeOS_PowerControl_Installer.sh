4e36fc138aae1e7cdeb7b33e6b28a3d7d1e72a7d
shadowed1 <itszachg@gmail.com>
2025-06-07 17:53:43 -0400

    Update ChromeOS_PowerControl_Installer.sh

--- ChromeOS_PowerControl_Installer.sh
+++ ChromeOS_PowerControl_Installer.sh
@@ -36,7 +36,7 @@ detect_cpu_type() {
 }
 echo ""
 echo "${RED}${BOLD}VT-2 (or enable sudo in crosh) is required to run this installer.$RESET"
-echo "${YELLOW}${BOLD}Must be installed in a location without the${RESET}${MAGENTA}${BOLD}noexec mount.$RESET"
+echo "${YELLOW}${BOLD}Must be installed in a location without the ${RESET}${MAGENTA}${BOLD}noexec mount.$RESET"
 echo ""
 read -rp "${GREEN}${BOLD}Enter desired Install Path - leave blank for default: /usr/local/bin/ChromeOS_PowerControl:$RESET" INSTALL_DIR
 INSTALL_DIR="${INSTALL_DIR:-/usr/local/bin/ChromeOS_PowerControl}"
