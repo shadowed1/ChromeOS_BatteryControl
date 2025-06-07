b640241b941d6edf17ce4b1627ce08894c982a53
shadowed1 <itszachg@gmail.com>
2025-06-07 16:51:31 -0400

    Update ChromeOS_PowerControl_Installer.sh

--- ChromeOS_PowerControl_Installer.sh
+++ ChromeOS_PowerControl_Installer.sh
@@ -194,7 +194,7 @@ echo "     ${GREEN}██${RESET}              ${YELLOW}██${RESET}     "
 echo "       ${GREEN}████${RESET}        ${YELLOW}████${RESET}       "
 echo "           ${GREEN}████████████${RESET}           "
 echo ""
-echo "${BOLD}${GREEN}Chrome${RESET}${BOLD}${RED}OS${RESET}${BOLD}${YELLOW}_${RESET}${BOLD}${CYAN}PowerControl${RESET}"
+echo "        ${BOLD}${GREEN}Chrome${RESET}${BOLD}${RED}OS${RESET}${BOLD}${YELLOW}_${RESET}${BOLD}${CYAN}PowerControl${RESET}"
 echo ""
 
 echo ""
